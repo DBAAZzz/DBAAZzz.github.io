20d753033ffa1e2c196b6ed322e06a922bd45a31
钟传德 <chuande.zhong@zhenai.com>
2025-06-06 19:14:45 +0800

    update

--- docs/.vitepress/config/constants.ts
+++ docs/.vitepress/config/constants.ts
@@ -4,7 +4,7 @@ export const metaData = {
   lang: 'zh-CN',
   locale: 'zh_CN',
   title: 'DBAAZzz的知识库',
-  description: '个人技术知识库，记录 & 分享个人碎片化、结构化、体系化的技术知识内容。',
+  description: '个人技术知识库，记录个人碎片化、结构化、体系化的技术知识内容。',
   site,
   image: `${site}/logo.jpg`,
 };
